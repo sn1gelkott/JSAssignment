197e04104749b90b075409191a7676047e812682
sn1gelkott <snigelkott@Miriams-MacBook-Pro.local>
2025-11-25 14:42:23 +0100

    P5Operations

---
+++ P4Variables/js/app.js
@@ -0,0 +1,9 @@
+let NumberOfBottles = 10;
+const line1= "green bottles hanging on the wall";
+const line2= "and if one green bottle, should accidentally fall, there will be";
+
+
+console.log(NumberOfBottles, line1);
+--NumberOfBottles; 1;
+console.log(NumberOfBottles, line2);
+console.log(NumberOfBottles, line1);
---
+++ P5Operators/js/app.js
@@ -0,0 +1,41 @@
+//: Example 1: Input for km/h to m/s
+let input = 72;
+let output = 20;
+let x= input/output;
+
+console.log(x)//: The conversion factor x = 3.6
+
+//: Example 2: Number of input in minutes to seconds
+let minutes = 3;
+let seconds = 180;
+let y = seconds/minutes;
+
+console.log(y); //: The conversion factor y = 60
+
+
+//: Example 3: Display division result of 2 numbers (11 and 4)
+let a = 11;
+let b = 4;
+let c = 11/4;
+
+console.log(c); //: The result c = 2.75
+
+//: Example 4: Display the remainder of 2 numbers 11/4:
+
+let modulo = a % b;
+
+console.log(modulo); //: The resulting remainder is 3.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
