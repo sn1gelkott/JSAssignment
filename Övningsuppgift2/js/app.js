8999bdbf0324edaeae82b9ec4004ce760d1c8647
sn1gelkott <snigelkott@Mac.lan>
2025-11-28 11:39:06 +0100

    Swag

---
+++ Övningsuppgift2/js/app.js
@@ -0,0 +1,9 @@
+let customerName = "Ragnhild"
+let customerAge = undefined
+if (customerAge > 15) {
+  console.log("Here's your coffee, have a nice day!")
+}
+
+if (customerAge < 15) {
+  console.log("You really shouldn't be ingesting caffeine at your age")
+}
